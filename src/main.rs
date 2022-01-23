d87ac08a36bcc69f685adb09c0c0a52ff3a76245
GabrielDertoni <gab.dertoni@gmail.com>
2022-01-23 19:30:07 -0300

    Use never type to remove `unreachable!()`

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@
 #![no_main]
 #![no_std]
 
-#![feature(fmt_internals)]
-#![feature(extern_types)]
-#![feature(format_args_nl)]
+#![feature(fmt_internals, extern_types, format_args_nl, never_type)]
 
 mod utils;
 mod boot;
@@ -15,7 +13,6 @@ mod print;
 mod globals;
 
 use core::panic::PanicInfo;
-use core::fmt::{ self, Write };
 
 unsafe fn kernel_init() -> ! {
     let gpio = drivers::GPIORegisters::get();
@@ -24,10 +21,9 @@ unsafe fn kernel_init() -> ! {
     globals::IS_MINI_UART_SETUP = true;
 
     kernel_main().unwrap();
-    unreachable!();
 }
 
-unsafe fn kernel_main() -> fmt::Result {
+unsafe fn kernel_main() -> Result<!, ()> {
     let mini_uart = drivers::MiniUARTRegisters::get();
 
     mu_println!("Initializing kernel...");
@@ -46,8 +42,7 @@ unsafe fn kernel_main() -> fmt::Result {
 fn panic(info: &PanicInfo) -> ! {
     unsafe {
         if globals::IS_MINI_UART_SETUP {
-            let mini_uart = drivers::MiniUARTRegisters::get();
-            writeln!(mini_uart, "{}", info).unwrap_unchecked();
+            mu_println!("{}", info);
         }
     }
     utils::inifinite_loop();
